5467bcabf494a221f19cfe4ffc1eaca7a79b73bb
cryptozeny <cryptozeny@gmail.com>
2018-03-29 16:11:04 +0900

    cd src && git grep -l 'litecoin' | xargs sed -i 's/litecoin/anzucoin/g'

--- src/init.cpp
+++ src/init.cpp
@@ -506,8 +506,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/litecoin-project/litecoin>";
-    const std::string URL_WEBSITE = "<https://litecoin.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/anzucoin-project/anzucoin>";
+    const std::string URL_WEBSITE = "<https://anzucoin.org>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2011, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
