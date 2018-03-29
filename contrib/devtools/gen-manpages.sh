6299da6ff9fd0684a3f9e00a8fa661b44445231b
cryptozeny <cryptozeny@gmail.com>
2018-03-29 17:25:22 +0900

    redo: git grep -l 'litecoin' | xargs sed -i 's/litecoin/anzucoin/g'

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -4,10 +4,10 @@ TOPDIR=${TOPDIR:-$(git rev-parse --show-toplevel)}
 SRCDIR=${SRCDIR:-$TOPDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$SRCDIR/litecoind}
-BITCOINCLI=${BITCOINCLI:-$SRCDIR/litecoin-cli}
-BITCOINTX=${BITCOINTX:-$SRCDIR/litecoin-tx}
-BITCOINQT=${BITCOINQT:-$SRCDIR/qt/litecoin-qt}
+BITCOIND=${BITCOIND:-$SRCDIR/anzucoind}
+BITCOINCLI=${BITCOINCLI:-$SRCDIR/anzucoin-cli}
+BITCOINTX=${BITCOINTX:-$SRCDIR/anzucoin-tx}
+BITCOINQT=${BITCOINQT:-$SRCDIR/qt/anzucoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$LITECOIND not found or not executable." && exit 1
 
